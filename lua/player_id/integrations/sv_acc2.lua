18da942528ac0eb63a6d841587e9f30189608e11
JTaylerJ <jtaylerj@gmail.com>
2025-12-04 10:33:00 -0800

    Make docs

--- lua/player_id/integrations/sv_acc2.lua
+++ lua/player_id/integrations/sv_acc2.lua
@@ -12,19 +12,3 @@ end)
 hook.Add("ACC2:Load:Character", "ACC2:Compatibilities:PlayerID:CharacterLoad", function(ply, characterId)
     hook.Run("playerID:OnCharacterLoad", characterId, ply)
 end)
-
-
--- Player when character is created
-hook.Add("playerID:OnCharacterCreated", "playerID:LogCharacterCreated", function(characterId, ply)
-    print("[playerID] Character created with ID:", characterId, "for player:", ply:Nick())
-end)
-
--- Player when character is saved
-hook.Add("playerID:OnCharacterSaved", "playerID:LogCharacterSaved", function(characterId, ply)
-    print("[playerID] Character saved with ID:", characterId, "for player:", ply:Nick())
-end)
-
--- Player when character is loaded
-hook.Add("playerID:OnCharacterLoad", "playerID:LogCharacterLoad", function(characterId, ply)
-    print("[playerID] Character loaded with ID:", characterId, "for player:", ply:Nick())
-end)
